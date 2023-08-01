3cc2a5ea31325affd64d113179c064165b2cbcdc
Jefferson L. Alionco <Jeffersonalionco@gmail.com>
2023-08-01 09:00:28 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Olá Munndo!!")
+from flask import Flask #Biblioteca que ajudara na criação do site
+
+app = Flask(__name__) #chamamos a biblioteca para nosso codigo(PAGINA)
+
+@app.route("/") # Configurando a rota do site.
+def index():
+    return "<b> Olá Mundo!<b>" # Aqui retornamos a pagina HTML, que estara na pasta templates
