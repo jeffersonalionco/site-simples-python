04444a6e7ac5d9674a4b4f5c2716181f6a1ff7cd
Jefferson L. Alionco <Jeffersonalionco@gmail.com>
2023-08-01 14:13:04 -0300

    contato, sobre, inicio modificado

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from flask import Flask, render_template #Biblioteca que ajudara na criação do
 app = Flask(__name__) #chamamos a biblioteca para nosso codigo(PAGINA)
 
 @app.route("/") # Configurando a rota do site.
+@app.route("/index.html")
 def index():
     return render_template('index.html')# Aqui retornamos a pagina HTML, que estara na pasta templates
+
+@app.route("/contato.html") # Configurando a rota do site.
+def contato():
+    return render_template('contato.html')# Aqui retornamos a pagina HTML, que estara na pasta templates
+
+@app.route("/about.html") # Configurando a rota do site.
+def about():
+    return render_template('about.html')# Aqui retornamos a pagina HTML, que estara na pasta templates
