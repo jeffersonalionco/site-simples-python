f8ec82eba70112afe4d330b24bb9cc2add3ec35f
Jefferson L. Alionco <Jeffersonalionco@gmail.com>
2023-08-01 09:13:08 -0300

    Adicionado Templates, e Index.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask #Biblioteca que ajudara na criação do site
+from flask import Flask, render_template #Biblioteca que ajudara na criação do site
 
 app = Flask(__name__) #chamamos a biblioteca para nosso codigo(PAGINA)
 
 @app.route("/") # Configurando a rota do site.
 def index():
-    return "<b> Olá Mundo!<b>" # Aqui retornamos a pagina HTML, que estara na pasta templates
+    return render_template('index.html')# Aqui retornamos a pagina HTML, que estara na pasta templates
